ef3ef136a6ca61af775efbcedfcade9ccbf50ff9
TECH2GO\mhenning <matze_henning@gmx.de>
2017-10-11 11:35:20 +0200

    Works

--- Program.cs
+++ Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace dirHash
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var path = PreparePath(args);
+
+                var xorHashesConten = CalcHash(path);
+
+                Console.WriteLine(xorHashesConten.ToLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static string PreparePath(string[] args)
+        {
+            var path = args[0];
+
+            if (!path.EndsWith(@"\"))
+            {
+                path = path + "\\";
+            }
+
+            return path;
+        }
+
+        private static byte[] CalcHash(string path)
+        {
+            var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            var hashesContent =
+                files.AsParallel().Select(f => new MD5CryptoServiceProvider().ComputeHash(File.ReadAllBytes(f)));
+            var xorHashesContent = hashesContent.XOR();
+
+            var relPathes = files.AsParallel().Select(item => new Uri(path).MakeRelativeUri(new Uri(item)).ToString());
+            var xorPathHashes = relPathes
+                .Select(p => Encoding.UTF8.GetBytes(p))
+                .Select(p => new MD5CryptoServiceProvider().ComputeHash(p))
+                .XOR();
+
+
+            return xorHashesContent.XOR(xorPathHashes);
         }
     }
 }
---
+++ XORStuff.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dirHash
+{
+    public static class XORStuff
+    {
+        public static byte[] XOR(this IEnumerable<byte[]> list)
+        {
+            return list.Aggregate((acc, item) => acc == null ? item : acc.XOR(item));
+        }
+
+
+        public static byte[] XOR(this byte[] left, byte[] right)
+        {
+            if (left.Length != right.Length)
+            {
+                throw new Exception("Error: left and right arrays lengths must be equal.");
+            }
+
+            return left.Zip(right, (a, b) => (byte)(a ^ b)).ToArray();
+        }
+
+        public static string ToLine(this byte[] left)
+        {
+            return left.Aggregate("", (acc, item) => acc + " " + item.ToString("X2"));
+        }
+    }
+}
