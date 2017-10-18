df44cde73ab7166dd29de71fb97161cdf68d216f
TECH2GO\mhenning <matze_henning@gmx.de>
2017-10-18 14:07:31 +0200

    Added fileHash

---
+++ fileHash/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileHash
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args == null || args.Length == 0 || !File.Exists(args[0]))
+                {
+                    throw new ArgumentException("Wrong number or kind of args!");
+                }
+
+                var file = File.ReadAllBytes(args[0]);
+
+                var result = new MD5CryptoServiceProvider().ComputeHash(file);
+
+                var output = result.Aggregate("", (acc, item) => acc + " " + item.ToString("X2"));
+
+                Console.WriteLine(output);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
